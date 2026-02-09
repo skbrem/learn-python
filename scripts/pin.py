84dc2739254bba8c75bd26b6a8ab6f235b17e587
Sean Bremner <sean@bremner.me>
2026-02-09 17:51:25 +0200

    new files

--- scripts/pin.py
+++ scripts/pin.py
@@ -1,6 +1,21 @@
+attempts = 0
+
 while True:
-    code = input("Pleases type in your pin: ")
+    code = input("Please type in your PIN: ")
+    attempts += 1
+
     if code == "1234":
+        success = True
         break
-    print("Incorrect... Try again")
-print("Correct PIN entered!")
+
+    if attempts == 3:
+        success = False
+        break
+
+    # This is printed if the code was incorrect and there have been less than 3 attempts 
+    print("Incorrect...try again!")
+
+if success:
+    print("Correct PIN entered!")
+else:
+    print("Too many attempts...")
