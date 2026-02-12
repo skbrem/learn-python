7976e7e28f05e5d4eed0c3fd37046ab04a914d37
Sean Bremner <sean@bremner.me>
2026-02-12 17:24:56 +0200

    advanced a little bit

--- code/chapters-1-and-2/working_with_numbers.py
+++ code/chapters-1-and-2/working_with_numbers.py
@@ -1,13 +1,26 @@
-numbers = 0
+total = 0
+count = 0
+positive = 0
+negative = 0
+
+print("Please type in integer numbers. Type in 0 to finish. ")
 
 while True:
-    num = int(input("Please type in integer number. Type in 0 to finish: "))
+    num = int(input(""))
     if num == 0:
         break
-    print(f"Number: {num}")
-    numbers = num + 1
+    total += num
+    count += 1
 
-print(f"The program asks for numbers...")
-print(f"Number: {num}")
-print(f"Numbers typed in: {numbers}")
+    if num > 0:
+        positive += 1
+    elif num < 0:
+        negative += 1
+
+print(f"...the program asks for numbers...")
+print(f"Numbers typed in {count}")
+print(f"The sum of the numbers is {total}")
+print(f"The mean of the numbers is {(total / count)}")
+print(f"Positive numbers {positive}")
+print(f"Negative numbers {negative}")
 
