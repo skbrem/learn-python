dcfe54f1e01b82f740af135f7c038389b717ce15
Sean Bremner <sean@bremner.me>
2026-02-17 10:29:44 +0200

    new scripts and notes

---
+++ code/chapter-3/less_than_ten.py
@@ -0,0 +1,7 @@
+number = int(input("Please type in a number: "))
+
+while number < 10:
+    print(number)
+    number += 1
+
+print("Execution finished.")
---
+++ code/chapter-3/simple-count.py
@@ -0,0 +1,6 @@
+a = 1
+while True:
+    print(a)
+    a += 1
+    if a == 5:
+        break
